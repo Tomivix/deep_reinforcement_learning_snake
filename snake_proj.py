42e573bbb701426dc3d96a33b35b444383d8c76a
Tomivix <tomivix@wp.pl>
2020-12-08 03:19:36 +0900

    druga wersja

--- snake_proj.py
+++ snake_proj.py
@@ -7,6 +7,7 @@ import tensorflow as tf
 import time
 import os
 import random
+from tqdm import tqdm
 
 MEMORY_FRACTION = 0.20 # useful to train multiple snakes
 
@@ -54,23 +55,88 @@ grid_pixels = grid_object.grid
 snakes_array = game_controller.snakes
 snake_object1 = snakes_array[0]
 
+# For stats
+ep_rewards = [-200]
 
+MIN_REWARD = -1000
 
-done = False
-for _ in range(1000):  # run for 1000 steps
+# Iterate over episodes
+for episode in tqdm(range(1, EPISODES + 1), ascii=True, unit='episodes'):
+
     env.render()  # Render latest instance of game
-    action = env.action_space.sample()  # Random action
-    print('action -> ', action)
 
-    #print('action -> ', action)
-    last_obs, rewards, done, info = env.step(action)  # Implement action
-    print('last_obs', last_obs)
-    print('last_obs.shape', last_obs.shape)
-    print('reward', rewards)
-    print('done', done)
-    print('info', info)
-    print('===================')
-    #time.sleep(10)
-    if done:
-        env.reset()
+    # Update tensorboard step every episode
+    agent.tensorboard.step = episode
+
+    # Restarting episode - reset episode reward and step number
+    episode_reward = 0
+    step = 1
+
+    # Reset environment and get initial state
+    current_state = env.reset()
+
+    # Reset flag and start iterating until episode ends
+    done = False
+    while not done:
+
+        # This part stays mostly the same, the change is to query a model for Q values
+        if np.random.random() > epsilon:
+            # Get action from Q table
+            action = np.argmax(agent.get_qs(current_state))
+        else:
+            # Get random action
+            action = np.random.randint(0, env.ACTION_SPACE_SIZE)
+
+        # print('action -> ', action)
+        # last_obs, rewards, done, info = env.step(action)  # Implement action
+        # print('last_obs', last_obs)
+        # print('last_obs.shape', last_obs.shape)
+        # print('reward', rewards)
+        # print('done', done)
+        # print('info', info)
+        # print('===================')
+        # time.sleep(10)
+        # if done:
+        #     env.reset()
+
+        new_state, reward, done = env.step(action)
+
+        # Transform new continous state to new discrete state and count reward
+        episode_reward += reward
+
+        if SHOW_PREVIEW and not episode % AGGREGATE_STATS_EVERY:
+            env.render()
+
+        # Every step we update replay memory and train main network
+        agent.update_replay_memory((current_state, action, reward, new_state, done))
+        agent.train(done, step)
+
+        current_state = new_state
+        step += 1
+
+    # Append episode reward to a list and log stats (every given number of episodes)
+    ep_rewards.append(episode_reward)
+    if not episode % AGGREGATE_STATS_EVERY or episode == 1:
+        average_reward = sum(ep_rewards[-AGGREGATE_STATS_EVERY:])/len(ep_rewards[-AGGREGATE_STATS_EVERY:])
+        min_reward = min(ep_rewards[-AGGREGATE_STATS_EVERY:])
+        max_reward = max(ep_rewards[-AGGREGATE_STATS_EVERY:])
+        agent.tensorboard.update_stats(reward_avg=average_reward, reward_min=min_reward, reward_max=max_reward, epsilon=epsilon)
+
+        # Save model, but only when min reward is greater or equal a set value
+        if min_reward >= MIN_REWARD:
+            agent.model.save(f'models/{MODEL_NAME}__{max_reward:_>7.2f}max_{average_reward:_>7.2f}avg_{min_reward:_>7.2f}min__{int(time.time())}.model')
+
+    # Decay epsilon
+    if epsilon > MIN_EPSILON:
+        epsilon *= EPSILON_DECAY
+        epsilon = max(MIN_EPSILON, epsilon)
+
 env.close()
+
+
+# done = False
+# for _ in range(1000):  # run for 1000 steps
+#
+#     action = env.action_space.sample()  # Random action
+#
+
